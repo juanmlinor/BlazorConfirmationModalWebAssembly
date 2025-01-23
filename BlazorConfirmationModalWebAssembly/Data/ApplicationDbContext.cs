496557bd90b92bfea6bbad857daa64d04daa8fae
JuanLino <jumalire55@outlook.com>
2025-01-22 21:33:38 -0800

    Add project files.

---
+++ BlazorConfirmationModalWebAssembly/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BlazorConfirmationModalWebAssembly.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BlazorConfirmationModalWebAssembly.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer(@"Data Source=(localdb)\\MSSQLLocalDB\DemoModal.db");
+        public DbSet<Employee> employees { get; set; }
+    }
+}
---
+++ BlazorConfirmationModalWebAssembly/Services/IEmployeeService.cs
@@ -0,0 +1,22 @@
+﻿using BlazorConfirmationModalWebAssembly.Data;
+using BlazorConfirmationModalWebAssembly.Models;
+
+namespace BlazorConfirmationModalWebAssembly.Services
+{
+    public interface IEmployeeService
+    {
+        Task addNewEmployeeAsync(Employee emloyee);
+    }
+
+    public class EmployeeService : IEmployeeService
+    {
+        public async Task addNewEmployeeAsync(Employee emloyee)
+        {
+            using (var dbcontext = new ApplicationDbContext())
+            {
+                await dbcontext.employees.AddAsync(emloyee);
+                await dbcontext.SaveChangesAsync();
+            }
+        }
+    }
+}
